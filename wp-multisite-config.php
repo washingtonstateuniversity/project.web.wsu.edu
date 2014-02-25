c0fcf129f0f7f73303e604e131c0ee6abef1094f
jeremyfelt <jeremy.felt@gmail.com>
2014-02-24 17:04:14 -0800

    Don't force the domain and path in multisite config

--- wp-multisite-config.php
+++ wp-multisite-config.php
@@ -3,7 +3,3 @@
 define( 'WP_ALLOW_MULTISITE', true );
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'dev.project.web.wsu.edu');
-define('PATH_CURRENT_SITE', '/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
