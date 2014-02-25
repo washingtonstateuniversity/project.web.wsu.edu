b8b7bdf1111d32a5a071b239c7be3783e52be9f3
jeremyfelt <jeremy.felt@gmail.com>
2014-02-24 16:56:25 -0800

    Add initial files for project.web.wsu

---
+++ wp-multisite-config.php
@@ -0,0 +1,9 @@
+<?php
+
+define( 'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'dev.project.web.wsu.edu');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
