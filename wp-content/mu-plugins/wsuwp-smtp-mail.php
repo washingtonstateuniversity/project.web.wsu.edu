65cee13ecf9b1ee4197ac264f283d15c0557a5d2
jeremyfelt <jeremy.felt@gmail.com>
2014-03-12 10:09:30 -0700

    Update notification email to show network and site name

--- wp-content/mu-plugins/wsuwp-smtp-mail.php
+++ wp-content/mu-plugins/wsuwp-smtp-mail.php
@@ -14,7 +14,7 @@ add_action( 'phpmailer_init', 'wsuwp_smtp_email' );
 function wsuwp_smtp_email( $phpmailer ) {
 	$phpmailer->Mailer = 'smtp';
 	$phpmailer->From = 'www-data@wsuwp-indie-prod-01.web.wsu.edu';
-	$phpmailer->FromName = 'WSU Projects - ' . esc_html( get_option( 'blogname' ) );
+	$phpmailer->FromName =  esc_html( get_current_site()->site_name ) . esc_html( get_option( 'blogname' ) );
 	$phpmailer->Sender = $phpmailer->From;
 	$phpmailer->Host = 'smtp.wsu.edu';
 	$phpmailer->Port = 25;
