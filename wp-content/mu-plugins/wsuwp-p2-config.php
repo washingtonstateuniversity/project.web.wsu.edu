f1ab925f4e20bb2d185ec56eb16d2d0a9138c0dd
jeremyfelt <jeremy.felt@gmail.com>
2014-03-12 09:22:53 -0700

    Move P2 config filters to common file

--- wp-content/mu-plugins/wsuwp-p2-config.php
+++ wp-content/mu-plugins/wsuwp-p2-config.php
@@ -16,3 +16,32 @@ Version: 0.1
  */
 add_filter( 'pre_option_comment_moderation', '__return_false' );
 add_filter( 'pre_option_comment_whitelist', '__return_false' );
+
+add_filter( 'upload_mimes', 'wsu_project_upload_mimes', 10, 1 );
+/**
+ * Additional mime types required by project.web.wsu.edu
+ *
+ * @param array $types Current mime types supported.
+ *
+ * @return array Modified list of mime types.
+ */
+function wsu_project_upload_mimes( $types ) {
+	$types['ai']  = 'application/postscript';
+	$types['psd'] = 'image/psd';
+	$types['eps'] = 'application/postscript';
+
+	return $types;
+}
+
+add_filter( 'pre_site_option_upload_filetypes', 'wsu_project_upload_filetypes', 10 );
+/**
+ * Set the allowed list of upload filetypes.
+ *
+ * Removed - midi, mid
+ * Added - pdf, ai, psd, eps, doc, xls, zip
+ *
+ * @return string Allowed upload filetypes on the network.
+ */
+function wsu_project_upload_filetypes() {
+	return 'jpg jpeg png gif mp3 mov avi wmv pdf ai psd eps doc xls zip';
+}
